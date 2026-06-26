7b5d5fc2e22380de368c0c76b85d9f8f9fc19d2e
vijaysurya-05 <vijaysuryahsn@gmail.com>
2026-06-26 11:52:36 +0530

    Intial code commit

--- pdf_agentic_chatbot.py
+++ pdf_agentic_chatbot.py
@@ -4,138 +4,87 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain.agents import AgentExecutor, create_tool_calling_agent
 from langchain_core.tools import tool
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.agents import AgentExecutor, create_tool_calling_agent
 import requests
-#streamlit run example_strlit.py
 
-llm = ChatGroq(
-        api_key=st.secrets["GROQ_API_KEY"],
-        model="openai/gpt-oss-120b",
-        streaming=False,
-        )
+def extract_text(uploaded_file):
+    data = uploaded_file.read()
+    doc = fitz.open(stream=data, filetype="pdf")
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text
 
-# Math tools
-@tool
-def add(a: int, b: int) -> int:
-    """Adds two numbers."""
-    return a + b
 
-@tool
-def multiply(a: int, b: int) -> int:
-    """Multiplies two numbers."""
-    return a * b
+def split_chunks(text):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    return splitter.split_text(text)
 
-@tool
-def subtract(a: int, b: int) -> int:
-    """Subtracts two numbers."""
-    return a - b
 
-# Web search tool
-@tool
-def web_search(query: str) -> str:
-        """Search the web using Tinyfish. Use this to get current information accurately."""
-        url = "https://api.search.tinyfish.ai"
+def store_chunks(chunks):
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L12-v2",
+        model_kwargs={"device": "cpu"},
+    )
+    vectorstore = Chroma.from_texts(
+        texts=chunks,
+        embedding=embeddings,
+        persist_directory="pdf_Agentic_db",
+    )
+    return vectorstore
+
+def build_agent(vectorstore):
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
+    @tool
+    def search_pdf(query: str) -> str:
+        """Search the uploaded PDF for relevant information."""
+        docs = retriever.invoke(query)
+        if not docs:
+            return "NO_CONTEXT_FOUND"
+        return "\n\n".join([doc.page_content for doc in docs])
+    @tool
+    def web_search(query: str) -> str:
+        """Search the web using Tinyfish. Use this only if search_pdf returns NO_CONTEXT_FOUND."""
+        url = "https://api.search.tinyfish.ai"  # or tavily
         headers = {"X-API-Key": st.secrets["TINYFISH_API_KEY"]}
         params = {"query": query}
         
         response = requests.get(url, headers=headers, params=params)
-        results = response.json()
+        results = response.json() #creates a dictionary from the json response
         
         output = ""
         for r in results.get("results", []):
             output += f"Title: {r.get('title', '')}\nSummary: {r.get('snippet', '')}\nURL: {r.get('url', '')}\n\n"
         
         return output if output else "No results found."
-# All tools together
-tools = [add, multiply, subtract, web_search]
-
-# Prompt
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a helpful assistant. Use tools when needed. Use the web_search tool only when you need current or external information."),
-    ("human", "{input}"),
-    ("placeholder", "{agent_scratchpad}"),
-])
-
-# Agent
-agent = create_tool_calling_agent(llm, tools, prompt)
-
-# Executor
-agent_executor = AgentExecutor(
-    agent=agent,
-    tools=tools,
-    verbose=True,
-    handle_parsing_errors=True
-)
-
-
-def extract_text(pdf_file):
-    doc = fitz.open(stream=pdf_file.read(), filetype="pdf")
-    text = ""
-
-    for page in doc:
-        text += page.get_text()
-
-    return text
-
-
-def split_chunks(text):
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
-        chunk_overlap=50
+    tools = [search_pdf, web_search]
+    llm = ChatGroq(
+        model="openai/gpt-oss-120b",
+        api_key="st.secrets["GROQ_API_KEY"]",
+        temperature=0
     )
-    return splitter.split_text(text)
-
-def store_chunks(chunks):
-    embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L12-v2",
-        model_kwargs={"device": "cpu"},
-        cache_folder=".cache/huggingface/hub"
-    )
-    vectorstore = Chroma.from_texts(
-        texts = chunks,
-        embedding=embeddings,
-        persist_directory=None
-        )
-    return vectorstore
-
-
-
-def ask(query,vectorstore):
-    retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
+    
     prompt = ChatPromptTemplate.from_messages([
-        (
-            "system",
-            "You are a helpful assistant. Answer questions using only the context provided."
-            "List ALL points mentioned in the context without skipping or summarizing any of them. "
-            "Use markdown formatting — bullet points for lists, code blocks for code. "
-            "Do not list everything in the context — only what is relevant to the question asked."
-            "Do not add any closing or summary sentences at the end of your response."
-            ),
-            (
-                "human",
-                "Context:\n{context}\n\nQuestion: {question}"
-                )
-                ])
-    chain = prompt | llm | StrOutputParser()
-    docs = retriever.invoke(query)
+        ("system", 
+         "You are a helpful assistant. ALways use search_pdf first"
+         "If it returns NO_CONTEXT_FOUND then use web_search"
+         "Decorate your answer with bullet points and give detailed information"
+         "Current year is 2026"),
+        ("human", "{input}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ])
+    agents=create_tool_calling_agent(llm, tools, prompt)
+    return AgentExecutor(
+        agent=agents,
+        tools=tools,
+        verbose=True #shows the reasoning steps of the agent
+    )
     
-    results = vectorstore.similarity_search_with_score(query, k=5)
-    best_score = results[0][1]
-    
-    scores = [score for doc, score in results]
-    print(f"All scores: {scores}")
-    print(f"Best score: {results[0][1]}")
-    
-    if best_score < 1.2: # Threshold for relevance
-        context = "\n\n".join([doc.page_content for doc, score in results])
-        return chain.invoke({"context": context, "question": query})  
-    else:
-        return agent_executor.invoke({"input": query})["output"]
 
-st.title("Chat with your PDF")
+# UI
+st.title("Smart PDF & Web Search Agent")
 
 uploaded_file = st.file_uploader("Upload a PDF", type="pdf")
 
@@ -143,15 +92,16 @@ if uploaded_file:
     with st.spinner("Reading and indexing your PDF..."):
         text = extract_text(uploaded_file)
         chunks = split_chunks(text)
-        collection = store_chunks(chunks)
+        vectorstore = store_chunks(chunks)
+        st.session_state["agent"] = build_agent(vectorstore)
     st.success(f"Ready! Indexed {len(chunks)} chunks.")
 
-    query = st.text_input("Ask a question about your PDF")
+question = st.text_input("Ask a question")
 
-    if query:
+if question:
+    if "agent" not in st.session_state:
+        st.warning("Please upload a PDF first.")
+    else:
         with st.spinner("Thinking..."):
-            answer = ask(query, collection)
-        st.write(answer)
-        
-# learn about "with" function
-# caching in streamlit to avoid loading every time
+             result = st.session_state["agent"].invoke({"input": question})
+             st.write(result["output"])
